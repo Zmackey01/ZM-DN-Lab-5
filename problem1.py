f7168719ab02ce312da4385f14f7fcc9457eb279
Zmackey01 <zmackey01@saintmarys.edu>
2026-09-21 16:02:33 -0400

    come back later

--- problem1.py
+++ problem1.py
@@ -1,12 +1,26 @@
 def ugly_number(n):
   """
-  ## add more to the docstring, including additional tests
+  number -> Boolean
   
+  takes n and returns True if it has a prime factor of 2, 3, 5 or none of these at all, returns False if otherwise
+
   >>> ugly_number(6)
   True
   >>> ugly_numer(1)
   True
   >>> ugly_number(14)
   False
+  >>> ugly_number(8)
+  True
   """
-  ### add your code here
+  if n%2==1:
+    return True
+  if n%3==1:
+    return True
+  if n%5==1:
+    return True
+  else:
+   return False
+    
+  
+  
