025024a1cc45d7d267bd9a8ab8ede5ac6a8e56b5
Zmackey01 <zmackey01@saintmarys.edu>
2026-09-24 20:45:51 -0400

    done

--- problem4.py
+++ problem4.py
@@ -1,12 +1,18 @@
 def power_of_three(n):
   """
-  ### add to the docstring, including doctests
+  number -> string
+
+  takes an integer and returns True if it is a power of three and False otherwise.
 
   >>> power_of_three(27)
   True
   >>> power_of_three(0)
-  ### what should be the answer here?
+  False
   >>> power_of_three(-10)
   False
   """
-  ## add your code here
+  if n <= 0:
+    return False
+  while n%3==0:
+    n//=3
+  return n==1
